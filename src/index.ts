d055e04410d9145c19b7de6a24c11834bcaf2a6e
Jakub Biesiada <biesiadajakub@icloud.com>
2020-03-13 22:10:46 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { exec } from 'child_process';
 
 const npmPackageInfo = async (name: string) => {
-  exec(`npm view ${name}`);
+  exec(
+    `npm view ${name} repository dependencies contributors versions deprecated`,
+    (error, stdout, stderr) => {
+      console.log(stdout);
+    }
+  );
 };
 
 export default npmPackageInfo;
