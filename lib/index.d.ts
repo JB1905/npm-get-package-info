38836b72e068eb0203cda96003a6de088f94ea05
Jakub Biesiada <biesiadajakub@icloud.com>
2020-03-28 19:01:17 +0100

    Update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-declare const npmPackageInfo: (name: string, version?: string | undefined) => Promise<void | {
+declare const npmGetPackageInfo: (name: string, version?: string | undefined) => Promise<void | {
     [key: string]: string;
 }>;
-export default npmPackageInfo;
+export default npmGetPackageInfo;
