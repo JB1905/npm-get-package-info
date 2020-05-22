5cfc174621da9c1252554c6b66777055388408a9
Jakub Biesiada <biesiadajakub@icloud.com>
2020-05-22 19:01:09 +0200

    refactor: clean up

--- README.md
+++ README.md
@@ -4,15 +4,19 @@
 [![NPM downloads](http://img.shields.io/npm/dm/npm-get-package-info.svg?style=flat-square)](https://www.npmjs.com/package/npm-get-package-info)
 
 ## About
+
 Get info about npm package
 
 ## How to Install
+
 First, install the library in your project by npm:
+
 ```sh
 $ npm install npm-get-package-info
 ```
 
 Or Yarn:
+
 ```sh
 $ yarn add npm-get-package-info
 ```
@@ -26,12 +30,14 @@ const info = await npmGetPackageInfo({
 ```
 
 ## Options
-Name | Type | Default | Description | Available options
--|-|-|-|-
-**name** | string | `undefined` | Package name | e.g.: `react`
-**version** | string | `undefined` | Package version | e.g.: `16.8.0`, `~2.1.2`, `^4.5.0`
-**parseOutput** | boolean | `true` | Parse results to JSON | Enable `true` / disable `false`
-**info** | string[] or string | package.json fields keys | Info about packages to be fetched | e.g.: `version`, `contributors`, `main` etc.
+
+| Name            | Type               | Default                  | Description                       | Available options                            |
+| --------------- | ------------------ | ------------------------ | --------------------------------- | -------------------------------------------- |
+| **name**        | string             | `undefined`              | Package name                      | e.g.: `react`                                |
+| **version**     | string             | `undefined`              | Package version                   | e.g.: `16.8.0`, `~2.1.2`, `^4.5.0`           |
+| **parseOutput** | boolean            | `true`                   | Parse results to JSON             | Enable `true` / disable `false`              |
+| **info**        | string[] or string | package.json fields keys | Info about packages to be fetched | e.g.: `version`, `contributors`, `main` etc. |
 
 ## License
+
 This project is licensed under the MIT License © 2020-present Jakub Biesiada
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,123 +1,123 @@
-import npmGetPackageInfo from "../src";
+import npmGetPackageInfo from '../src';
 
-describe("package info", () => {
-  it("should return package info for the latest react version", async () => {
+describe('package info', () => {
+  it('should return package info for the latest react version', async () => {
     const info = await npmGetPackageInfo({
-      name: "react",
+      name: 'react',
     });
 
     if (!Array.isArray(info)) {
-      expect(info.name).toBe("react");
+      expect(info.name).toBe('react');
     }
   });
 
-  it("should return package info for @angular/cli@8.0.0", async () => {
+  it('should return package info for @angular/cli@8.0.0', async () => {
     const info = await npmGetPackageInfo({
-      name: "@angular/cli",
-      version: "8.0.0",
+      name: '@angular/cli',
+      version: '8.0.0',
     });
 
     if (!Array.isArray(info)) {
-      expect(info.description).toBe("CLI tool for Angular");
-      expect(info.version).toBe("8.0.0");
+      expect(info.description).toBe('CLI tool for Angular');
+      expect(info.version).toBe('8.0.0');
     }
   });
 
-  it("should return stringified license & description for the latest vue version", async () => {
+  it('should return stringified license & description for the latest vue version', async () => {
     const info = await npmGetPackageInfo({
-      name: "vue",
+      name: 'vue',
       parseOutput: false,
-      info: ["license", "description"],
+      info: ['license', 'description'],
     });
 
     expect(info).toContain('"license":"MIT"');
   });
 
-  it("should return info for versions <16.10.0, 16.11.0)", async () => {
+  it('should return info for versions <16.10.0, 16.11.0)', async () => {
     const info = await npmGetPackageInfo({
-      name: "react",
-      version: "~16.10.0",
+      name: 'react',
+      version: '~16.10.0',
     });
 
-    const items = ["16.10.0", "16.10.1", "16.10.2"];
+    const items = ['16.10.0', '16.10.1', '16.10.2'];
 
     info.map(({ version }: any, index: number) => {
       expect(version).toBe(items[index]);
     });
   });
 
-  it("should return object with license for versions <16.10.0, 16.11.0)", async () => {
+  it('should return object with license for versions <16.10.0, 16.11.0)', async () => {
     const info = await npmGetPackageInfo({
-      name: "universal-tilt.js",
-      version: "~2.0.0",
-      info: ["license"],
+      name: 'universal-tilt.js',
+      version: '~2.0.0',
+      info: ['license'],
     });
 
     info.map(({ license }: any) => {
-      expect(license).toBe("MIT");
+      expect(license).toBe('MIT');
     });
   });
 
-  it("should return license for versions <16.10.0, 16.11.0)", async () => {
+  it('should return license for versions <16.10.0, 16.11.0)', async () => {
     const info = await npmGetPackageInfo({
-      name: "react",
-      version: "~16.10.0",
-      info: "license",
+      name: 'react',
+      version: '~16.10.0',
+      info: 'license',
     });
 
     info.map((license: string) => {
-      expect(license).toBe("MIT");
+      expect(license).toBe('MIT');
     });
   });
 
-  it("should not return data for incorrect package version", async () => {
+  it('should not return data for incorrect package version', async () => {
     try {
       await npmGetPackageInfo({
-        name: "jquery",
-        version: "0.0.0",
+        name: 'jquery',
+        version: '0.0.0',
         parseOutput: false,
       });
     } catch (err) {
-      expect(err.message).toBe("Data not found for provided package");
+      expect(err.message).toBe('Data not found for provided package');
     }
   });
 
-  it("should return throw an error", async () => {
+  it('should return throw an error', async () => {
     try {
       await npmGetPackageInfo({
-        name: "lorem-ipsum-dolor-sit-amet",
+        name: 'lorem-ipsum-dolor-sit-amet',
       });
     } catch (err) {
-      expect(err.name).toBe("Error");
+      expect(err.name).toBe('Error');
     }
   });
 
-  it("should return name for latest package version", async () => {
+  it('should return name for latest package version', async () => {
     const info = await npmGetPackageInfo({
-      name: "react",
+      name: 'react',
       parseOutput: false,
-      info: ["name"],
+      info: ['name'],
     });
 
     expect(info).toMatch(`"react"`);
   });
 
-  it("should return undefined as value for `Rick Astley`", async () => {
+  it('should return undefined as value for `Rick Astley`', async () => {
     const info = await npmGetPackageInfo({
-      name: "react",
-      info: ["Rick Astley", "name"],
+      name: 'react',
+      info: ['Rick Astley', 'name'],
     });
 
-    expect(info["Rick Astley"]).toBe(undefined);
+    expect(info['Rick Astley']).toBe(undefined);
   });
 
-  it("should return version for react", async () => {
+  it('should return version for react', async () => {
     const info = await npmGetPackageInfo({
-      name: "react",
-      version: "16.8.0",
-      info: "version",
+      name: 'react',
+      version: '16.8.0',
+      info: 'version',
     });
 
-    expect(info).toBe("16.8.0");
+    expect(info).toBe('16.8.0');
   });
 });
