f939f88c2d9a8eec9c9b7c246fb8576342d45007
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-20 21:56:32 +0200

    test: add mocks

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,38 @@
 import npmGetPackageInfo from '../src';
 
+import * as child_process from 'child_process';
+
+import reactLatest from './__mocks__/results/react-latest.json';
+import angularVersion from './__mocks__/results/angular-version.json';
+import vueLatest from './__mocks__/results/vue-latest.json';
+import reactRange from './__mocks__/results/react-range.json';
+import universalTiltRange from './__mocks__/results/universal-tilt-range.json';
+import reactVersion from './__mocks__/results/react-version.json';
+
+jest.mock('child_process');
+
+const mockedResults = {
+  'npm view react --json': { stdout: JSON.stringify(reactLatest) },
+  'npm view @angular/cli@8.0.0 --json': {
+    stdout: JSON.stringify(angularVersion),
+  },
+  'npm view vue --json': { stdout: JSON.stringify(vueLatest) },
+  'npm view react@~16.10.0 --json': { stdout: JSON.stringify(reactRange) },
+  'npm view universal-tilt.js@~2.0.0 --json': {
+    stdout: JSON.stringify(universalTiltRange),
+  },
+  'npm view jquery@0.0.0 --json': { stdout: '' },
+  'npm view lorem-ipsum-dolor-sit-amet --json': { stdout: '' },
+  'npm view react@16.8.0 --json': { stdout: JSON.stringify(reactVersion) },
+  'npm view @angular/cli --json': { stderr: new Error(), stdout: null },
+} as any;
+
+const mockedExec = jest.spyOn(child_process, 'exec');
+
+mockedExec.mockImplementation((command: string, callback: any) =>
+  callback(null, mockedResults[command])
+);
+
 describe('package info', () => {
   it('should return package info for the latest react version', async () => {
     const info = await npmGetPackageInfo({
@@ -82,7 +115,7 @@ describe('package info', () => {
     }
   });
 
-  it('should return throw an error', async () => {
+  it('should throw an error', async () => {
     try {
       await npmGetPackageInfo({
         name: 'lorem-ipsum-dolor-sit-amet',
@@ -119,5 +152,15 @@ describe('package info', () => {
     });
 
     expect(info).toBe('16.8.0');
+  });
+
+  it('should throw an error', async () => {
+    try {
+      await npmGetPackageInfo({
+        name: '@angular/cli',
+      });
+    } catch (err) {
+      expect(err.name).toBe('Error');
+    }
   });
 });
